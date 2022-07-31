fa6d07f8b538de74b96abaee24b310bd22d44153
Patrick.Degner <Acaer@gmx.net>
2022-07-31 10:25:15 +0200

    Fix for loop counter of new entry's

--- ETL/Load.py
+++ ETL/Load.py
@@ -6,10 +6,9 @@ conn = sqlite3.connect('news.db')
 # Get counter for loop from count in staging DWH
 def get_counter_staging():
     count = conn.execute(
-        'SELECT COUNT(*) FROM news_staging '
-        'WHERE ((SELECT MAX(Datum||" "||Zeit) FROM news_staging) > '
-        '(SELECT CASE WHEN MAX(Datum_ID||" "||Zeit) '
-        'IS NULL THEN "2022-01-01 00:00" ELSE MAX(Datum_ID||" "||Zeit) END FROM news_fact))')
+        'SELECT COUNT(((Datum||" "||Zeit))) FROM news_staging '
+        'WHERE (Datum||" "||Zeit) > (SELECT CASE WHEN MAX(Datum_ID||" "||Zeit) IS NULL '
+        'THEN "2022-01-01 00:00" ELSE MAX(Datum_ID||" "||Zeit) END FROM news_fact)')
     conn.commit()
     return count.fetchone()[0]
 
@@ -29,7 +28,6 @@ def load():
         conn.execute(
             f'INSERT INTO news_dim(News_FK, Titel) '
             f'SELECT last_insert_rowid(), Titel FROM news_staging LIMIT 1 OFFSET {counter}'
-
         )
         conn.commit()
         counter += 1
